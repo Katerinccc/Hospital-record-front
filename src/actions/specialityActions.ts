f29adb0ec79b09f03128dacda4872cad415d9a71
Katerin Calderon <kccc0713@gmail.com>
2022-07-28 12:05:28 -0500

    add model interface of speciality, add async functions to get data from the APIs

---
+++ src/actions/specialityActions.ts
@@ -0,0 +1,51 @@
+import { ISpeciality } from './../models/ISpeciality';
+
+const API = 'http://localhost:8080/api/v1/speciality/';
+
+export async function getAllSpecialties() {
+    const response:Response = await fetch(API + "list");
+    const dataApi:ISpeciality[] = await response.json();
+    return dataApi;
+}
+
+export async function getSpeciality(id:number) {
+    const response:Response = await fetch(API + id);
+    const dataApi:ISpeciality = await response.json();
+    return dataApi;
+}
+
+export async function createSpeciality(speciality:ISpeciality){
+    const response:Response = await fetch(API, {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json' 
+        },
+        body: JSON.stringify(speciality)
+    })
+    const dataApi:ISpeciality = await response.json();
+    return dataApi;
+}
+
+export async function updateSpeciality(id:number, speciality:ISpeciality){
+    const response:Response = await fetch((API + id), {
+        method: 'PUT',
+        headers: {
+        'Content-Type': 'application/json' 
+        },
+        body: JSON.stringify(speciality)
+    })
+    const dataApi:ISpeciality = await response.json();
+    return dataApi;
+}
+
+export async function deleteSpeciality(id:number){
+    const response:Response = await fetch( (API + id), {
+        method: "DELETE",
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        },
+        body: 'application/json'
+    })
+    const dataApi:boolean = await response.json();
+    return dataApi;
+}
